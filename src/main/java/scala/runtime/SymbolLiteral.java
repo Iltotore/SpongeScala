09dcfbe4b547bb79fc7411b655e0a2e550850a89
Iltotore <rafbodaha@gmail.com>
2020-11-22 10:38:11 +0100

    Fix a scala-compatibility issue between plugins' dep and Forge's
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

---
+++ src/main/java/scala/runtime/SymbolLiteral.java
@@ -0,0 +1,33 @@
+/*
+ * Relocated Scala patch
+ *
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.runtime;
+
+import java.lang.invoke.*;
+
+public final class SymbolLiteral {
+    private SymbolLiteral() {
+    }
+
+    public static CallSite bootstrap(MethodHandles.Lookup lookup, String invokedName,
+                                     MethodType invokedType,
+                                     String value) throws Throwable {
+        ClassLoader classLoader = lookup.lookupClass().getClassLoader();
+        MethodType type = MethodType.fromMethodDescriptorString("(Ljava/lang/String;)Lio/github/iltotore/spongescala/library/Symbol;", classLoader);
+        Class<?> symbolClass = Class.forName("io.github.iltotore.spongescala.library.Symbol", false, classLoader);
+        MethodHandle factoryMethod = lookup.findStatic(symbolClass, "apply", type);
+        Object symbolValue = factoryMethod.invokeWithArguments(value);
+        return new ConstantCallSite(MethodHandles.constant(symbolClass, symbolValue));
+    }
+}
